00970ddd48103d158dcb2e6c4083682ff8851a76
marcamsler1 <marctheodorfelix.amsler@uzh.ch>
2025-12-03 16:35:27 +0100

    Display snake head

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use bevy::prelude::*;
+use bevy::sprite_render::{Wireframe2dConfig, Wireframe2dPlugin};
+
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            Wireframe2dPlugin::default(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, toggle_wireframe)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2d);
+
+    let Snakehead = meshes.add(Circle::new(50.0));
+
+    let color = Color::hsl(360.0, 1.0, 50.0);
+
+    commands.spawn((
+        Mesh2d(Snakehead),
+        MeshMaterial2d(materials.add(color)),
+    ));
+}
+
+fn toggle_wireframe(
+    mut wireframe_config: ResMut<Wireframe2dConfig>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        wireframe_config.global = !wireframe_config.global;
+    }
 }
