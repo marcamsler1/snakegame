5ad9b816a6a3b25c209eeca7f6a6f5ea2d4871ec
marcamsler1 <marctheodorfelix.amsler@uzh.ch>
2025-12-04 12:06:55 +0100

    slapped the grid on there

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use bevy::prelude::*;
 use bevy::sprite_render::{Wireframe2dConfig, Wireframe2dPlugin};
+use bevy::window::PrimaryWindow;
 
+const GRID_WIDTH: u32 = 15;
+const GRID_HEIGHT: u32 = 15;
 
-fn main() {
-    App::new()
-        .add_plugins((
-            DefaultPlugins,
-            Wireframe2dPlugin::default(),
-        ))
-        .add_systems(Startup, setup)
-        .add_systems(Update, (toggle_wireframe, snake_movement))
-        .run();
+#[derive(Component, Clone, Copy, PartialEq, Eq)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Size {
+    width: f32,
+    height: f32,
+}
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
 }
 
 #[derive(Component)]
@@ -21,6 +33,22 @@ enum Direction {
     Down,
 }
 
+#[derive(Component)]
+struct SnakeHead;
+
+fn main() {
+    App::new()
+        .add_plugins((
+            DefaultPlugins,
+            Wireframe2dPlugin::default(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (toggle_wireframe, snake_movement))
+        .add_systems(PostUpdate, (position_translation, size_scaling))
+        .run();
+}
+
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -28,17 +56,18 @@ fn setup(
 ) {
     commands.spawn(Camera2d);
 
-    let snakehead = meshes.add(Circle::new(20.0));
-
+    let mesh = meshes.add(Circle::new(0.5));
     let color = Color::srgb(193.0/255.0, 196.0/255.0, 0.0/255.0);
 
     commands.spawn((
-        Mesh2d(snakehead),
+        Mesh2d(mesh),
         MeshMaterial2d(materials.add(color)),
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        GlobalTransform::default(),
+        Position { x: 3, y: 3 },
+        Size::square(1.0),
+        SnakeHead,
         Direction::Right,
-
+        Transform::default(),
+        GlobalTransform::default(),
     ));
 }
 
@@ -53,21 +82,58 @@ fn toggle_wireframe(
 
 fn snake_movement(
     keyboard_input: Res<ButtonInput<KeyCode>>, 
-    mut head_position: Query<(&mut Direction, &mut Transform)>
+    mut query: Query<&mut Position, With<SnakeHead>>,
 ) {
-    for (_head, mut transform) in &mut head_position {
+    for mut pos in &mut query {
         if keyboard_input.just_pressed(KeyCode::ArrowLeft) {
-            transform.translation.x -= 50.;
+            pos.x -= 1;
         }
         if keyboard_input.just_pressed(KeyCode::ArrowRight) {
-            transform.translation.x += 50.;
+            pos.x += 1;
         }
         if keyboard_input.just_pressed(KeyCode::ArrowDown) {
-            transform.translation.y -= 50.;
+            pos.y -= 1;
         }
         if keyboard_input.just_pressed(KeyCode::ArrowUp) {
-            transform.translation.y += 50.;
+            pos.y += 1;
         }
 
+        pos.x = (pos.x.rem_euclid(GRID_WIDTH as i32));
+        pos.y = (pos.y.rem_euclid(GRID_HEIGHT as i32));
+
+    }
+}
+
+fn size_scaling(
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut q: Query<(&Size, &mut Transform)>,
+) {
+    let window = windows.single().expect("no primary window");
+    for (size, mut transform) in &mut q {
+        transform.scale = Vec3::new(
+            size.width / GRID_WIDTH as f32 * window.width(),
+            size.height / GRID_HEIGHT as f32 * window.height(),
+            1.0,
+        );
+    }
+}
+
+fn position_translation(
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut q: Query<(&Position, &mut Transform)>,
+) {
+    let window = windows.single().expect("no primary window");
+
+    fn convert(pos: f32, window: f32, grid: f32) -> f32 {
+        let tile_size = window / grid;
+        pos / grid * window - window / 2. + tile_size / 2.
+    }
+
+    for (pos, mut transform) in &mut q {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width(), GRID_WIDTH as f32),
+            convert(pos.y as f32, window.height(), GRID_HEIGHT as f32),
+            0.0,
+        );
     }
 }
